9567f6e532d68378c01e13c53eac0f1502ec0d6f
唐凯泽 <703652767@qq.com>
2019-11-24 12:22:02 +0800

    hello world

---
+++ src/main/java/com/atguigu/day1/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.atguigu.day1;
+
+/**
+ * @Auther:
+ * @Date: 2019/11/24
+ * @Descprition
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
