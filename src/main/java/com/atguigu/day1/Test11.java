7ffa79bac3bbcab54a7bfd0deac46905f9bd2d55
李将 <lijiang@daling.com>
2019-11-24 20:02:59 +0800

    提交代码

--- src/main/java/com/atguigu/day1/Test11.java
+++ src/main/java/com/atguigu/day1/Test11.java
@@ -3,12 +3,13 @@ package com.atguigu.day1;
 /**
  * @Auther: 唐凯泽
  * @Date: 2019/11/24
- * @Descprition: 1、基本数据类型 8种
+ * @Descprition:
+ * 1、基本数据类型 8种
  * 1）整型：
- * byte： 字节类型
- * short:  短整型
- * int :  整型
- * long: 长整型
+ * byte： 字节类型     1个字节
+ * short:  短整型      2个字节
+ * int :  整型        4个字节
+ * long: 长整型       8个字节
  * 2)浮点型：
  * float:
  * double:
@@ -18,7 +19,19 @@ package com.atguigu.day1;
  * boolean
  * 2、引用数据类型：类、接口、数组、枚举........
  * 例如： String、System、Math、Date这些都是类
+ *
+ * 计算机最小的存储单位是bit，比特，位
+ * 计算机最基本的存储单位是byte，字节。 1个字节 = 8位
+ * 1kb ： 1024byte
+ * 1mb ： 1024kb
+ * 1gb : 1024mb
+ * 1tb : 1024gb
  */
 public class Test11 {
+
+    public static void main(String[] args) {
+
+        System.out.println();
+    }
 
 }
