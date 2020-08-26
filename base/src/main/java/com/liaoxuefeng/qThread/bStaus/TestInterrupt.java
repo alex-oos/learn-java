6ed069ad78230ab0acfd340a8f5f373bde398644
李将 <lijiang@aixuexi.com>
2020-08-26 18:29:32 +0800

    中断线程

--- base/src/main/java/com/liaoxuefeng/qThread/bStaus/TestInterrupt.java
+++ base/src/main/java/com/liaoxuefeng/qThread/bStaus/TestInterrupt.java
@@ -3,20 +3,55 @@ package com.liaoxuefeng.qThread.bStaus;
 /**
  * @Auther: 李将
  * @Date: 2020/7/18 15:45
- * @Descprition:
+ * @Descprition: interrupt()  中断线程  目标线程需要反复检测自身状态是否是interrupted状态，如果是，就立刻结束运行。
  */
 public class TestInterrupt {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws InterruptedException {
+        Thread t = new Mythread();
+        t.start();
+        Thread.sleep(1); //暂停1豪秒
+        t.interrupt(); //中断t线程
+        t.join(); // 等待t线程结束
+        System.out.println("end");
     }
 }
 
 class Mythread extends Thread {
 
-    int n
     @Override
     public void run() {
-        super.run();
+        Thread hello = new HelloThread();
+        hello.start();
+        try {
+            hello.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        hello.interrupt();
     }
+}
+
+class HelloThread extends Mythread {
+
+
+    @Override
+    public void run() {
+
+        int n = 0;
+        while (!isInterrupted()) {
+            n++;
+            System.out.println(n + " hello");
+
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+//                e.printStackTrace();
+                break;
+            }
+        }
+
+    }
+
+    ;
 }
 
