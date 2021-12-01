bce1d0873ff53b6010efb7ec2bd9da6e2586cd6f
Alex <Alexl.i@qq.com>
2021-12-02 07:42:38 +0800

    数组算法

---
+++ base/src/main/java/com/atguigu/day7/ArrayException.java
@@ -0,0 +1,39 @@
+package com.atguigu.day7;
+
+
+/**
+ * @author Alex
+ * @date 2021/12/2 7:12 上午
+ * <P>
+ * <p>
+ * * 数组中的常见异常：
+ * * 1. 数组角标越界的异常：ArrayIndexOutOfBoundsExcetion
+ * * 2. 空指针异常：NullPointerException
+ * </p>
+ */
+public class ArrayException {
+
+    public static void main(String[] args) {
+        // 1. 数组角标越界的异常：ArrayIndexOutOfBoundsExcetion
+        int[] arr1 = new int[]{1, 3, 4, 5};
+        // System.out.println(arr1[5]);
+        //   2. 空指针异常：NullPointerException
+
+
+        // 	int[] arr1 = new int[]{1,2,3};
+//		arr1 = null;
+//		System.out.println(arr1[0]);
+
+        //情况二：
+//		int[][] arr2 = new int[4][];
+//		System.out.println(arr2[0][0]);
+
+        //情况三：
+        String[] arr3 = new String[]{"AA", "BB", "CC"};
+        arr3[0] = null;
+        System.out.println(arr3[0].toString());
+
+
+    }
+
+}
---
+++ base/src/main/java/com/atguigu/day7/BubbleSortTest.java
@@ -0,0 +1,40 @@
+package com.atguigu.day7;
+
+
+import java.util.Arrays;
+
+/**
+ * @author Alex
+ * @date 2021/11/26 5:29 下午
+ * <P>
+ * 冒泡排序</p>
+ */
+public class BubbleSortTest {
+
+    public static void main(String[] args) {
+        int[] array = new int[10];
+        //随机生成十个不重复的元素
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 100);
+            for (int j = 0; j < i; j++) {
+                if (array[i] == array[j]) {
+                    i--;
+                    break;
+                }
+            }
+        }
+        //进行冒泡排序
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - 1 - i; j++) {
+                if (array[j] > array[j + 1]) {
+                    int tmp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = tmp;
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(array));
+    }
+
+}
