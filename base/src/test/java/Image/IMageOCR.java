842437b0100970a21b149101f16793bfb10e7405
alex <46887064+alex-oos@users.noreply.github.com>
2023-07-12 14:28:30 +0800

    add 图鉴地址
    Signed-off-by: alex <46887064+alex-oos@users.noreply.github.com>

--- base/src/test/java/Image/IMageOCR.java
+++ base/src/test/java/Image/IMageOCR.java
@@ -17,7 +17,7 @@ import java.util.Map;
  * @Description :  OCR验证码图片识别
  * 百度OCR: 一年之内免费识别500次
  * 阿里云OCR:免费量（剩余/月赠）: 200/200次
- * 图鉴OCR: 使用一次0.002 元
+ * 图鉴OCR: 使用一次0.002 元 http://www.ttshitu.com/docs/java.html
  **/
 public class IMageOCR {
     /**
