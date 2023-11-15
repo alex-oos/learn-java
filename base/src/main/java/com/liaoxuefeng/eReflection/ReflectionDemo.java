1f64c3651a24e5861b66dc49d3a6cd2c49f14d7b
Alex <46887064+alex-oos@users.noreply.github.com>
2023-11-15 16:54:04 +0800

    delete:无用代码
    Signed-off-by: Alex <46887064+alex-oos@users.noreply.github.com>

--- base/src/main/java/com/liaoxuefeng/eReflection/ReflectionDemo.java
+++ base/src/main/java/com/liaoxuefeng/eReflection/ReflectionDemo.java
@@ -1,4 +1,4 @@
-package cn.javaguide.base.reflection;
+package com.liaoxuefeng.eReflection;
 
 /**
  * @ClassName ReflectionDemo
@@ -17,7 +17,7 @@ public class ReflectionDemo {
         System.out.println("方式一： " + a);
 
         // 方式二： 通过classname
-        Class alunbarClass1 = Class.forName("cn.javaguide.base.reflection.ReflectionDemo");
+        Class alunbarClass1 = Class.forName("com.liaoxuefeng.eReflection.ReflectionDemo");
         System.out.println("方式二：" + alunbarClass1);
 
         // 方式三：通过 类直接引用
@@ -26,7 +26,7 @@ public class ReflectionDemo {
         System.out.println("方式三： " + demoClass);
 
         // 方式四：通过类加载器直接获取
-        Object tmp = ClassLoader.getSystemClassLoader().loadClass("cn.javaguide.base.reflection.ReflectionDemo");
+        Object tmp = ClassLoader.getSystemClassLoader().loadClass("com.liaoxuefeng.eReflection.ReflectionDemo");
         System.out.println("方式四： " + tmp);
     }
 
