d941d73e7ba0c34b97a3d5628f55aa692b7b5453
Alex <46887064+alex-oos@users.noreply.github.com>
2023-11-15 18:31:51 +0800

    CompletableFuture补充
    Signed-off-by: Alex <46887064+alex-oos@users.noreply.github.com>

--- base/src/main/java/com/liaoxuefeng/qThread/eThreadPool/CompletableFutureLearning.java
+++ base/src/main/java/com/liaoxuefeng/qThread/eThreadPool/CompletableFutureLearning.java
@@ -69,22 +69,48 @@ public class CompletableFutureLearning {
 
     /**
      * completableFuture中常用的方法：
-     * 1. 使用thenApply()处理正常结果  thenApply() 可以使用 thenApply() 处理和改变CompletableFuture的结果。持有一个Function<R,T>作为参数。Function<R,T>是一个简单的函数式接口，接受一个T类型的参数，产出一个R类型的结果。
-     * 2. 使用exceptionally()处理异常结果
-     * 3. 使用thenAccept()处理正常结果但不返回值
+     * 1. 使用thenApply()处理正常结果
+     * thenApply() 可以使用 thenApply() 处理和改变CompletableFuture的结果。持有一个Function<R,T>作为参数。Function<R,T>是一个简单的函数式接口，接受一个T类型的参数，产出一个R类型的结果。
+     * 2. 使用thenAccept()处理正常结果但不返回值
+     * 3. 使用exceptionally()处理异常结果
      * 4. 使用handle()处理异常结果并返回值
      * 5. 使用thenCompose()实现多个CompletableFuture的串行执行
      * 6. 使用thenCombine()实现多个CompletableFuture的并行执行
      * 7. 使用allOf()实现多个CompletableFuture的并行执行
      * 8. 使用anyOf()实现多个CompletableFuture的并行执行
      */
-    public static void function1() {
+    public static void methods() throws ExecutionException, InterruptedException {
+
+        CompletableFuture<String> future = CompletableFuture.supplyAsync(() -> {
+            return "hello world";
+        }).thenApply(s -> {
+            return s.toUpperCase();
+        });
+        String s = future.get();
+        System.out.println("thenApply的用法" + s);
+
+        CompletableFuture<String> future1 = CompletableFuture.supplyAsync(() -> {
+            return "hello world";
+        });
+        // 获取异步执行结果，如果执行结果还没有返回，主线程就会阻塞等待
+        String unused = future1.get();
+        System.out.println("unused = " + unused);
+        // thenAccept() 也是接收一个Consumer<T>类型的参数，Consumer<T>是一个消费型的函数式接口，接受一个T类型的参数，不返回结果。
+        // 与thenApply()不同的是，thenAccept()不会将CompletableFuture的结果作为参数传递给该函数式接口，而是直接在CompletableFuture上应用该函数。
+        future1.thenAccept(s1 -> {
+            System.out.println("thenAccept的用法:" + s1);
+        });
+        // thenAccept 与get 方法的区别：thenAccept()是异步执行的，而get()是同步执行的，会阻塞等待结果返回
+        // thenAccept不会有线程的上下文切换。而get()方法会有线程的上下文切换。
+        // 当资源可以不受线程执行影响的时候，建议用thenAccept，比如netty中获取到结果后，通过channel发送发送消息，用thenAccept应该比get更优！
+
 
     }
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
 
         createCompletableFutureClass();
+        methods();
     }
 
 
