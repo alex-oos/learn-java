b3f9922196bc28cfda26ffaffb539d8d3c3eb1e2
Alex <46887064+alex-oos@users.noreply.github.com>
2024-04-08 15:26:18 +0800

    线程池笔记
    Signed-off-by: Alex <46887064+alex-oos@users.noreply.github.com>

--- springboot-learning/src/main/java/com/springboot/config/AsyncConfig.java
+++ springboot-learning/src/main/java/com/springboot/config/AsyncConfig.java
@@ -17,13 +17,25 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 @EnableAsync
 public class AsyncConfig {
 
+
+    private static final int CPU_COUNT = Runtime.getRuntime().availableProcessors();
+
+    private static final int CORE_POOL_SIZE = CPU_COUNT * 2;
+
+    private static final int MAX_POOL_SIZE = CPU_COUNT * 4;
+
+
     @Bean
     public TaskExecutor taskExecutor() {
         // 采用spring 中的线程池配置，优点是比自己配置线程池更简单
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(20);
-        executor.setMaxPoolSize(50);
+        // 设置核心线程数
+        executor.setCorePoolSize(CORE_POOL_SIZE);
+        // 设置最大线程数
+        executor.setMaxPoolSize(MAX_POOL_SIZE);
+        // 设置队列
         executor.setQueueCapacity(100);
+        // 设置线程池名称
         executor.setThreadNamePrefix("pipeline-async-");
         // 线程之间传递对象
         executor.setTaskDecorator(null);
